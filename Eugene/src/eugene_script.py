194cfed7afc34f5f3232ec7b1b72bd8ed9e5d39d
bruner <juip782@gmail.com>
2018-08-22 09:41:40 +0200

    Eugene: last few touchups on scripting

--- Eugene/src/eugene_script.py
+++ Eugene/src/eugene_script.py
@@ -1,18 +1,27 @@
 import re
 import sys
 import database
+import subprocess
 import eugene_modules
 import eugene_util
 
 from pprint import pprint
 
+class UnknownOperatorError(Exception):
+    pass
+
 # Regex for an s-expression (list of tokens wrapped in parentheses)
-_COMMAND = re.compile(r"\(\s*((?:[\w\\/\-.\*\+]+\s*)*)\)")
+_COMMAND = re.compile(r"\(\s*((?:[\w\\/\-.\*${}]+\s*)*)\)")
+
+# Regex for a variable -> ${}
+_VAR = re.compile(r"\${\s*(\w+)\s*}")
+
+_COMMENT_DELIM = "#"
 
 def _load_script(f):
     script = ""
     for line in f.readlines():
-        comment_pos = line.find('#')
+        comment_pos = line.find(_COMMENT_DELIM)
         if comment_pos < 0:
             script += line
             continue
@@ -25,10 +34,31 @@ def _load_script(f):
 def _expand_commands(commands, env):
     """Expands 'env' variables in place (mutates 'commands')"""
 
+    def expand_one(match):
+        var = match[1]   # First capture group -> stripped variable name
+        if var in env:
+            return env[var]
+
+        return var
+
     for cmd in commands:
         for i in range(1, len(cmd)): # Never expand the operator
-            expanded = map(lambda x: env[x] if x in env else x, cmd[i].split('+'))
-            cmd[i] = ''.join(expanded)
+            expanded = _VAR.sub(expand_one, cmd[i])
+            cmd[i] = expanded
+
+def _move_if_newer(args, last_command=None, **kwargs):
+    if last_command: return
+
+    result = subprocess.run(["move"] + args,
+        # capture_output=True to prevent out-of-order console output
+        capture_output=True, shell=True, encoding='utf-8')
+
+    print("")
+    print(result.stdout)
+
+_OPERATORS = {
+    "move-if-newer": _move_if_newer,
+}
 
 def main(db, args):
     script = None
@@ -40,23 +70,25 @@ def main(db, args):
         print("        ...exiting")
         sys.exit(-1)
 
-    env = map(lambda arg: arg.split('='), args[1:])
+    env = map(lambda arg: arg.strip().split('='), args[1:])
     env = { name: val for name, val in env }
-
-    print(f"script:\n{script}\nenv:")
-    pprint(env)
 
     commands = [ cmd.split() for cmd in _COMMAND.findall(script) if cmd ]
     _expand_commands(commands, env)
-    print(f"commands:")
-    pprint(commands)
 
     exit_code = -1
     for cmd in commands:
-        if not cmd: continue
+        if not cmd: continue   # Empty expression
 
         module = cmd[0]
         args = []
+        if module not in eugene_modules.MODULES:
+            if module not in _OPERATORS:
+                raise UnknownOperatorError()
+
+            _OPERATORS[module](cmd[1:], last_command=exit_code)
+            continue
+
         if module != "run":
             args = eugene_util.expand_args(cmd[1:])
         else:
@@ -64,4 +96,4 @@ def main(db, args):
 
         exit_code = eugene_modules.MODULES[module](db, args)
 
-    return exit_code
+    return 0
